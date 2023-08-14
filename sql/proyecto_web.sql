79d687483e299670c89b011fae8834ee778b2589
Jose Ricardo Imbaquinga Guaña <99418968+jrimbaquingaguana@users.noreply.github.com>
2023-08-13 22:08:15 -0500

    v2

--- sql/proyecto_web.sql
+++ sql/proyecto_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-08-2023 a las 03:30:02
+-- Tiempo de generación: 14-08-2023 a las 05:07:11
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -24,12 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `compra`
+--
+
+CREATE TABLE `compra` (
+  `ID_compra` int(11) NOT NULL,
+  `codigo_usuario` int(11) NOT NULL,
+  `codigo_inventario` int(11) NOT NULL,
+  `cantidadc` int(11) NOT NULL,
+  `precioc` double NOT NULL,
+  `fechac` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `compra`
+--
+
+INSERT INTO `compra` (`ID_compra`, `codigo_usuario`, `codigo_inventario`, `cantidadc`, `precioc`, `fechac`) VALUES
+(7, 1, 3, 1, 2.5, '2023-08-13 19:10:50'),
+(14, 1, 1, 1, 1, '2023-08-13 21:05:46'),
+(15, 1, 1, 1, 1, '2023-08-13 21:06:40'),
+(20, 1, 3, 2, 5, '2023-08-13 21:38:09');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `inventario`
 --
 
 CREATE TABLE `inventario` (
   `ID` int(11) NOT NULL,
-  `codigo` int(11) NOT NULL,
   `nombre` varchar(200) NOT NULL,
   `cantidad` int(11) NOT NULL,
   `precio` double NOT NULL,
@@ -41,13 +65,49 @@ CREATE TABLE `inventario` (
 -- Volcado de datos para la tabla `inventario`
 --
 
-INSERT INTO `inventario` (`ID`, `codigo`, `nombre`, `cantidad`, `precio`, `foto`, `fecha`) VALUES
-(1, 121, 'Madera Triplex', 12, 15, '../fotos/marvel.jpg', '2023-08-12 20:19:12'),
-(3, 121, 'Madera normal', 12, 4.5, '../fotos/foto.jpg', '2023-08-12 20:24:37');
+INSERT INTO `inventario` (`ID`, `nombre`, `cantidad`, `precio`, `foto`, `fecha`) VALUES
+(1, 'Madera Triplex', 103, 57.32, '../fotos/marvel.jpg', '2023-08-13 21:06:40'),
+(3, 'Madera normal', 52, 17.5, '../fotos/foto.jpg', '2023-08-13 21:38:09'),
+(9, '', 0, 0, '../fotos/pepa.jpg', '2023-08-13 21:51:16'),
+(10, '', 0, 0, '../fotos/pepa.jpg', '2023-08-13 21:54:03'),
+(11, '', 0, 0, '../fotos/pepa.jpg', '2023-08-13 21:55:55'),
+(12, '', 0, 1, '../fotos/marvel.jpg', '2023-08-13 21:58:01'),
+(13, 'pepa.jpg', 1, 1, '../fotos/pepa.jpg', '2023-08-13 22:00:01'),
+(14, 'pepa', 1, 1, '../fotos/pepa.jpg', '2023-08-13 22:00:53');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombreu` varchar(200) NOT NULL,
+  `constraseña` varchar(200) NOT NULL,
+  `rol` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombreu`, `constraseña`, `rol`) VALUES
+(1, 'Jose', '123', 'Bodegero'),
+(2, 'Pepe', '040500', 'Fabricador'),
+(3, 'Pedro', '040500', 'Administrador');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `compra`
+--
+ALTER TABLE `compra`
+  ADD PRIMARY KEY (`ID_compra`),
+  ADD KEY `codigo_inventario` (`codigo_inventario`),
+  ADD KEY `codigo_usuario` (`codigo_usuario`);
 
 --
 -- Indices de la tabla `inventario`
@@ -56,14 +116,43 @@ ALTER TABLE `inventario`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `compra`
+--
+ALTER TABLE `compra`
+  MODIFY `ID_compra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `inventario`
 --
 ALTER TABLE `inventario`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `compra`
+--
+ALTER TABLE `compra`
+  ADD CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`codigo_inventario`) REFERENCES `inventario` (`ID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `compra_ibfk_2` FOREIGN KEY (`codigo_usuario`) REFERENCES `usuarios` (`id_usuario`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
